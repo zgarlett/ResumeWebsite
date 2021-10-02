728477354bf6d694bcb90647c55ee7ccbb7feae0
zgarlett <zachary.garlett@okstate.edu>
2021-10-01 20:09:26 -0500

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Resume, Demo, Work } from './components';
+
+function App() {
+    return (
+        <>
+            <Demo />
+            <Resume />
+            <Work />
+        </>
+    );
+}
+
+export default App;
