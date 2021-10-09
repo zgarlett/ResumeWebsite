86090fb3b472019b58837f82913c5bc199aa29be
zgarlett <zachary.garlett@okstate.edu>
2021-10-09 18:20:33 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Resume, Demo, Work } from './components';
+import { Navigation } from './components/navigation';
 
 function App() {
     return (
         <>
+            <Navigation />
             <Demo />
             <Resume />
             <Work />
